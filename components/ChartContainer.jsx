d219b50510710de320fc25c96daec2a34885431f
emmaglorypraise <emmaglorypraise@gmail.com>
2022-07-03 22:29:57 +0100

    fixed navbar for small screen

--- components/ChartContainer.jsx
+++ components/ChartContainer.jsx
@@ -10,23 +10,23 @@ import WalletTransfer from "./Modal/WalletTransfer.jsx";
 const ChartContainer = () => {
     return (
         <div className='flex w-full  px-[10px] md:pl-[40px] md:pr-[40px] font-poppins'>
-            <div className="w-full md:w-2/2 flex flex-col md:flex-row gap-[12px] bg-[#FFF2FC] rounded-lg shadow-container py-[20px] px-[30px]">
+            <div className="w-full md:w-2/2 flex flex-col md:flex-row gap-[12px] bg-[#FFF2FC] rounded-lg shadow-container py-[20px] px-[30px] pt-[25px]">
 
-                <div className='md:border-r-[0.88px] md:border-r-[#C1C1C1] md:border-solid'>
+                <div className='w-full md:w-1/2 md:border-r-[0.88px] md:border-r-[#C1C1C1] md:border-solid  md:border-b-[0] border-b-[0.88px] border-b-[#C1C1C1] border-solid'>
                     <div className='flex flex-col gap-[20px]'>
                         <h3 className='text-[#9E2F92] text-lg font-semibold '>Porfolio</h3>
                         <span className='text-[#3A3737] text-xs font-medium'>Total balance</span>
                         <span className='text-[#676565] font-semibold text-2xl'>USD 50,345.28</span>
                     </div>
-                    <div className='flex pt-[70px] pb-[36px]'>
-                        <div className='py-[16px] pr-[16px] border-r-[0.88px] left-0 border-r-[#C1C1C1] border-solid flex flex-col'>
+                    <div className='flex pt-[70px] pb-[0px] mb:pb-[36px]'>
+                        <div className='w-1/2  py-[16px]  border-r-[0.88px] left-0 border-r-[#C1C1C1] border-solid flex flex-col '>
                             <div className='flex'>
                             <RiArrowUpLine color='#1E9940' fill='#1E9940' stroke='#1E9940'/>
                             <span className='text-[#3A373799] font-medium text-xs'>Income</span>
                             </div>
                             <span className='pt-[6px] text-[#676565] font-semibold text-sm'>USD 50,345.28</span>
                         </div>
-                        <div className='p-[16px] flex flex-col'>
+                        <div className='w-1/2 py-[16px] pl-[16px] md:pl-[86px] flex flex-col'>
                             <div className='flex'>
                             <RiArrowDownLine color='#F33030' fill='#F33030' stroke='#F33030' />
                             <span className='text-[#3A373799] font-medium text-xs'>Expenses</span>
@@ -37,10 +37,10 @@ const ChartContainer = () => {
 
                 </div>
 
-                <div className='flex flex-col justify-start'>
+                <div className='w-full md:w-1/2 mt-[25px] md:mt-[0px] flex flex-col justify-start'>
                     <div className='flex flex-col lg:flex-row justify-start md:justify-between'>
                     <div className="flex flex-col justify-center w-full md:w-1/3 md:ml-[20px]">
-                        <h3 className='text-[#9E2F92] text-lg font-semibold mr-[250px] text-center'>Wallet</h3>
+                        <h3 className='text-[#9E2F92] text-lg font-semibold  text-left'>Wallet</h3>
                         <div className='flex justify-start'> 
                             <Piechart />
                         </div>
@@ -110,103 +110,6 @@ const ChartContainer = () => {
                 </div>
                 
             </div>
-            {/* <div className="w-1/2 ml-[10px] flex gap-[12px] bg-[#FFF2FC] rounded-lg shadow-container py-[20px] px-[30px]">
-
-<div className='border-r-[0.88px] border-r-[#C1C1C1] border-solid'>
-    <div className='flex flex-col gap-[20px]'>
-        <h3 className='text-[#9E2F92] text-lg font-semibold '>Porfolio</h3>
-        <span className='text-[#3A3737] text-xs font-medium'>Total balance</span>
-        <span className='text-[#676565] font-semibold text-2xl'>USD 50,345.28</span>
-    </div>
-    <div className='flex pt-[70px] pb-[36px]'>
-        <div className='p-[16px] border-r-[0.88px] left-0 border-r-[#C1C1C1] border-solid flex flex-col'>
-            <div className='flex'>
-            <RiArrowUpLine color='#1E9940' fill='#1E9940' stroke='#1E9940'/>
-            <span className='text-[#3A373799] font-medium text-xs'>Income</span>
-            </div>
-            <span className='pt-[6px] text-[#676565] font-semibold text-sm'>USD 50,345.28</span>
-        </div>
-        <div className='p-[16px] flex flex-col'>
-            <div className='flex'>
-            <RiArrowDownLine color='#F33030' fill='#F33030' stroke='#F33030' />
-            <span className='text-[#3A373799] font-medium text-xs'>Expenses</span>
-            </div>
-            <span className='pt-[6px] text-[#676565] font-semibold text-sm'>USD 50,345.28</span>
-        </div>
-    </div>
-
-</div>
-
-<div className='flex flex-col'>
-    <div className='flex justify-between'>
-    <div className="flex flex-col justify-center w-1/3 ml-[20px]">
-        <h3 className='text-[#9E2F92] text-lg font-semibold mr-[250px] text-center'>Wallet</h3>
-        <div className='flex justify-start'> 
-            <Piechart />
-        </div>
-    </div>
-
-    <div className='flex flex-col justify-center w-2/3'>
-
-        <span className='text-end font-poppins text-[10.5274px] leading-[16px]'>3 currencies</span>
-        <div className='bg-[#F4F1F1] rounded-lg mt-[7px] ml-[21px] mr-[12px] mb-[18px] flex'>
-            <div className='p-[20px] w-full'>
-                <div className='flex justify-between mb-4 mr-auto w-full'>
-                    <div className='flex'>
-                    <img src={bitcoin} className='mb-[10px] mr-[10px]'  alt="bitcoin" />
-                    <div>
-                        <p className='font-poppins font-semibold text-[14.036px] leading-[21px] text-[off-black]'>BTC</p>
-                        <p className='font-poppins font-normal text-[8.77285px] leading-[13px] text-[off-grey]'>Bitcoin</p>
-                    </div>
-                    </div>
-                    <div className='flex'>
-                        
-                        <RiArrowUpLine color='#1E9940' fill='#1E9940' stroke='#1E9940'/>
-                        <p className='font-poppins font-normal text-[10.5274px] leading-[16px] text-[#1E9940]'>13.4%</p>
-                    </div>
-                </div>
-                <div className='flex justify-between mb-4 mr-auto w-full'>
-                <div className='flex'>
-                    <img src={ethereum} className='mb-[10px] mr-[10px]'  alt="ethereum" />
-                    <div>
-                        <p className='font-poppins font-semibold text-[14.036px] leading-[21px] text-[off-black]'>ETH</p>
-                        <p className='font-poppins font-normal text-[8.77285px] leading-[13px] text-[off-grey]'>Ethereum</p>
-                    </div>
-                </div>
-                <div className='flex'>
-                    
-                        <RiArrowUpLine color='#1E9940' fill='#1E9940' stroke='#1E9940'/>
-                        <p className='font-poppins font-normal text-[10.5274px] leading-[16px] text-[#1E9940]'>13.4%</p>
-                </div>
-                </div>
-                <div className='flex justify-between mb-4 mr-auto w-full'>
-                <div className='flex'>
-                    <img src={usdt} className='mb-[10px] mr-[10px]'  alt="usdt" />
-                    <div>
-                            <p className='font-poppins font-semibold text-[14.036px] leading-[21px] text-[off-black]'>USDT</p>
-                            <p className='font-poppins font-normal text-[8.77285px] leading-[13px] text-[off-grey]'>Tether</p>
-                    </div>
-                </div>
-                <div className='flex'>
-                    
-                        <RiArrowUpLine color='#1E9940' fill='#1E9940' stroke='#1E9940'/>
-                        <p className='font-poppins font-normal text-[10.5274px] leading-[16px] text-[#1E9940]'>13.4%</p>
-                </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    </div>
-
-    <div className='flex justify-center place-content-center mb-[30px]'>
-        <button className='p-[15px] font-poppins font-semibold text-[16.09px] leading-[24.1px] text-[#ffffff] bg-[#A22C90] shadow-button rounded-[19.636px]'>Make wallet tranfer</button>
-    </div>
-
-</div>
-
-            </div> */}
-
         </div>
     )
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -13,6 +13,7 @@ const Navbar = () => {
 
   const handleClick = () => {
     setActive(!active);
+    console.log('works')
   };
   return (
     <>
@@ -22,7 +23,7 @@ const Navbar = () => {
                 <Image src={logo} width="53.02"  height="53.04" alt="logo" /> 
         </div>
         </Link>
-        <button className=' inline-flex p-3 hover:bg-gray-900 rounded lg:hidden text-[#3A3737] ml-auto hover:text-white outline-none'
+        <button className=' inline-flex p-3 hover:bg-[#9E2F92] rounded lg:hidden text-[#9E2F92] ml-auto hover:text-white outline-none'
         onClick={handleClick}
         >
           <svg
@@ -43,88 +44,62 @@ const Navbar = () => {
         <div 
         className={`${
           active ? '' : 'hidden'
-        } hidden w-[90%] lg:flex lg:flex-grow lg:justify-between  lg:w-auto`}
+        } w-[90%] lg:flex lg:flex-grow lg:justify-between  lg:w-auto`}
         >
-            <div className='flex md:flex-row flex-col justify-evenly items-center md:ml-[20px] md:gap-[59px] font-normal text-base text-[#3A3737CC]'>
-                  <span className='font-medium text-[#3A3737]'><Link href="/">Dashboard</Link></span>
-                  <span><Link href="/calender">Calendar</Link></span>
-                  <span>Wallet</span>
-                  <span>Market</span>
+            <div className='flex lg:flex-row flex-col justify-evenly pl-[5px] mt-3 lg:mt-0  lg:items-center lg:ml-[20px] lg:gap-[59px] font-normal text-base text-[#3A3737CC]'>
+                  <span className='font-medium  mb-2 lg:mb-0'><Link href="/">Dashboard</Link></span>
+                  <span  className='font-medium mb-2 lg:mb-0'><Link href="/calender">Calendar</Link></span>
+                  <span  className='font-medium mb-2 lg:mb-0'>Wallet</span>
+                  <span  className='font-medium mb-2 lg:mb-0'>Market</span>
             </div>
-            <div className='flex items-center justify-end md:gap-[21px]'>
-         <IoMdNotifications size={20} />
-         <IoMdSettings size={20} onClick={() => setShowModal(true)} />
-        {showModal ? (
-           <>
-             <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-               <div className="relative w-auto  mx-auto max-w-3xl">
-                 <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                  <div className=" px-8 pb-2 mt-4  w-full pt-2 rounded-t ">
-                    <button
-                       className="bg-transparent border-0 mb-4 text-black float-right"
-                       onClick={() => setShowModal(false)}
-                     >
-                       <span className="text-black opacity-7 h-6 w-8 text-xl block py-0 rounded-full">
-                         x
-                       </span>
-                   </button>
-                  <div className="mt-4 flex flex-row justify-start">
-                  <Image src={settingIcon} className="px-2" width={76} alt="schedule Icon" />
-                  <div>
-                  <h3 className="text-center font-poppins font-semibold text-[30px] leading-[45px] text-[#3A3737]">Settings</h3>
-                  <p className="text-center font-poppins font-normal text-[10px] leading-[15px] text-off-grey">Set your preferences</p>
-                         </div>
-                         </div>
-                        
-                       </div>
-                       <div className="relative p-12 flex-auto">
-                         <form className="flex flex-row  px-3 pt-1 w-full">
-                           <div>
-                             <h3 className="font-poppins text-[20px] leading-[30px] font-medium text-off-grey">Email Notifications</h3>
-                         <p className="font-poppins text-[10px] leading-[15px] font-normal text-off-black">Receive email notifications of missed tasks and scheduled tasks</p>
-                         </div>
-                         <Toggle/>
-                       </form>
-                     </div>
-                   </div>
-                 </div>
-               </div>
-             </>
-           ) : null}
-           <span className='text-sm'>Clara Mary</span>
-           <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" className='rounded-full w-[24px] h-[24px]' alt="" />
-           <IoIosArrowDown size={20} />
-          {/* <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto text-[#3A3737CC]'>
-            <Link href='/'>
-              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-[#3A3737] font-bold items-center justify-center hover:bg-gray-600 hover:text-white '>
-                Home
-              </a>
-            </Link>
-            <Link href='/'>
-              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-[#3A3737] font-bold items-center justify-center hover:bg-gray-600 hover:text-white'>
-                Services
-              </a>
-            </Link>
-            <Link href='/'>
-              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-[#3A3737] font-bold items-center justify-center hover:bg-gray-600 hover:text-white'>
-                About us
-              </a>
-            </Link>
-            <Link href='/'>
-              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-[#3A3737] font-bold items-center justify-center hover:bg-gray-600 hover:text-white'>
-                Contact
-              </a>
-            </Link>
-          </div> */}
+            <div className='flex items-center justify-start my-3 lg:my-0 lg:justify-end lg:gap-[21px]'>
+              <IoMdNotifications className='mr-4 lg:mr-0' size={20} />
+              <IoMdSettings className='mr-4 lg:mr-0' size={20} onClick={() => setShowModal(true)} />
+              {showModal ? (
+                <>
+                  <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                    <div className="relative w-auto  mx-auto max-w-3xl">
+                      <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                        <div className=" px-8 pb-2 mt-4  w-full pt-2 rounded-t ">
+                          <button
+                            className="bg-transparent border-0 mb-4 text-black float-right"
+                            onClick={() => setShowModal(false)}
+                          >
+                            <span className="text-black opacity-7 h-6 w-8 text-xl block py-0 rounded-full">
+                              x
+                            </span>
+                              </button>
+                              <div className="mt-4 flex flex-row justify-start">
+                              <Image src={settingIcon} className="px-2" width={76} alt="schedule Icon" />
+                              <div>
+                              <h3 className="text-center font-poppins font-semibold text-[30px] leading-[45px] text-[#3A3737]">Settings</h3>
+                              <p className="text-center font-poppins font-normal text-[10px] leading-[15px] text-off-grey">Set your preferences</p>
+                              </div>
+                              </div>
+                              
+                            </div>
+                            <div className="relative p-12 flex-auto">
+                              <form className="flex flex-row  px-3 pt-1 w-full">
+                                <div>
+                                  <h3 className="font-poppins text-[20px] leading-[30px] font-medium text-off-grey">Email Notifications</h3>
+                                  <p className="font-poppins text-[10px] leading-[15px] font-normal text-off-black">Receive email notifications of missed tasks and scheduled tasks</p>
+                                  </div>
+                              <Toggle/>
+                            </form>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                ) : null}
+                <span className='text-sm mr-4' >Clara Mary</span>
+                <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" className='rounded-full w-[24px] h-[24px]' alt="" />
+                <IoIosArrowDown className='ml-4 lg:ml-0' size={20} />
             </div>
        </div>
       </nav>
     </>
   );
 };
-
-    // </>
-//   )
-// }
 
 export default Navbar
